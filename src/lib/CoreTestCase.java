1e89c75ebbef9e20f4a1419d6d62277458dfabc3
rusina <marina.rusina@reksoft.com>
2025-11-18 11:56:50 +0300

    18.11

--- src/lib/CoreTestCase.java
+++ src/lib/CoreTestCase.java
@@ -1,3 +1,4 @@
+//методы setUp и tearDown для всего проекта, методы работы с устройством для всего проекта
 package lib;
 
 import io.appium.java_client.AppiumDriver;
@@ -13,38 +14,40 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.URL;
 
 public class CoreTestCase extends TestCase {
+
     protected AppiumDriver driver;
 
-
-
+    //Метод переопределяет метод setUp из класса TestCase, автоматически вызывается перед выполнением
+    // каждого теста.
     @Override
     protected void setUp() throws Exception
     {
+        //Вызов базового метода класса TestCase
         super.setUp();
+        // В переменную driver записывается драйвер из метода getDriver класса Platform, обращение к синглтону
+        //гарантирует, что существует только 1 экземпляр класса Platform
         driver = Platform.getInstance().getDriver();
         this.rotateScreenPortrait();
-
     }
-
 
     @Override
     protected void tearDown() throws Exception
     {
-
         driver.quit();
         super.tearDown();
     }
 
+    //Метод для поворота экрана в портретную ориентацию
     protected void rotateScreenPortrait()
     {
         driver.rotate(ScreenOrientation.PORTRAIT);
     }
-
+    //Метод для поворота экрана в лэндскейп ориентацию
     protected void rotateScreenLandscape()
     {
         driver.rotate(ScreenOrientation.LANDSCAPE);
     }
-
+    //Метод для работы в бэкграунде
     protected void runAppInBackground(int seconds)
     {
         driver.runAppInBackground(5);
--- src/lib/ui/MainPageObject.java
+++ src/lib/ui/MainPageObject.java
@@ -14,122 +14,17 @@ import java.util.regex.Pattern;
 
 abstract public class MainPageObject {
     protected AppiumDriver driver;
-    protected RemoteWebDriver driver2;
-    protected static String
-            BUTTON_BACK;
+    protected static String BUTTON_BACK;
 
+    //Определение конструктора класса, вызывается автоматически при создании нового экземпляра
+    //класса наследника. Присваивает переменной driver значение AppiumDriver
     public MainPageObject(AppiumDriver driver)
     {
         this.driver = driver;
     }
-
-    public MainPageObject(RemoteWebDriver remoteWebDriver) {
-        this.driver2 = remoteWebDriver;
-
-    }
-
-    //выполнить Enter
-    public void sendEnter()
-    {
-        driver.executeScript("mobile:performEditorAction", ImmutableMap.of("action","done"));
-    }
-
-    //ожидание, что элемент не присутствует
-    public Boolean waitForElementNotPresent(String locator, String error, long timeOutInSeconds)
-    {
-        By by = this.getLocatorString(locator);
-        WebDriverWait wait = new WebDriverWait(driver,timeOutInSeconds);
-        wait.withMessage(error + "\n");
-        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
-    }
-
-    //ожидание, что элемент присутствует с выбором своего таймаута
-    public WebElement waitForElementPresent(String locator, String error, long timeOutInSeconds)
-    {
-        By by = this.getLocatorString(locator);
-        WebDriverWait wait = new WebDriverWait(driver,timeOutInSeconds);
-        wait.withMessage(error + "\n");
-        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
-    }
-
-    //дефолтное ожидание, что элемеент присутствует - время 5 сек
-    public WebElement waitForElementPresent(String locator, String error)
-    {
-        return waitForElementPresent(locator, error, 5);
-    }
-
-    //ожидание, что элемент присутствует и клик по нему
-    public WebElement waitForElementPresentAndClick(String locator, String error, long timeOutInSeconds)
-    {
-        WebElement element = waitForElementPresent(locator, error, timeOutInSeconds);
-        element.click();
-        return element;
-    }
-
-    //ожидание, что элемент присутствует и ввод строки
-    public WebElement waitForElementAndSendKeys(String locator, String value, String error, long timeOutInSeconds)
-    {
-        WebElement element = waitForElementPresent(locator, error, timeOutInSeconds);
-        element.sendKeys(value);
-        return element;
-    }
-
-    //ожидание, что элемент не присутствует и очистка строки
-    public WebElement waitForElementAndClear(String locator, String error, long timeOutInSeconds)
-    {
-        WebElement element = waitForElementPresent(locator, error, timeOutInSeconds);
-        element.clear();
-        return element;
-    }
-
-    public void swipeUP(int timeOut)
-    {
-        TouchAction action = new TouchAction(driver);
-        Dimension size = driver.manage().window().getSize();
-        int x = size.width/2;
-        int start_y = (int) (size.height * 0.8);
-        int end_y = (int) (size.height * 0.2);
-        action.press(x,start_y).waitAction(timeOut).moveTo(x,end_y).release().perform();
-    }
-
-    public void swipeUpQuick()
-    {
-        swipeUP(300);
-    }
-
-    public void swipeUpAndFindElement(String locator, String error, int max_swipes)
-    {
-        By by = this.getLocatorString(locator);
-        int already_swiped = 0;
-        while (driver.findElements(by).size() == 0) {
-            if (already_swiped > max_swipes)
-            {
-                waitForElementPresent(locator, "Cannot find element by swiping up. \n" + error, 0);
-                return;
-            }
-            swipeUpQuick();
-            ++already_swiped;
-        }
-
-    }
-
-    public void swipeLeft(String locator, String error) throws InterruptedException {
-        WebElement element = waitForElementPresent(locator, error,10);
-        int left_x = element.getLocation().getX();
-        int right_x = left_x + element.getSize().getWidth();
-        int upper_y = element.getLocation().getY();
-        int lower_y = upper_y + element.getSize().getHeight();
-        int middle_y = (upper_y + lower_y)/2;
-        TouchAction action = new TouchAction(driver);
-        action.press(right_x, middle_y).waitAction(150).moveTo(left_x,middle_y).release().perform().waitAction(900);
-    }
-
-    public void clickBackButton()
-    {
-        this.waitForElementPresentAndClick(BUTTON_BACK,
-                "Cannot click button Back", 10);
-    }
-
+    //Метод типа By принимает строку, разбивает ее на две части используя : и записывает первую часть в одну
+    // переменную, а вторую часть в другую переменную. Потом возвращает вторую часть с присанным By.xpath
+    // или By.id
     private By getLocatorString(String locator_with_type)
     {
         String[] exploded_locator = locator_with_type.split(Pattern.quote(":"), 2);
@@ -146,5 +41,124 @@ abstract public class MainPageObject {
             throw  new IllegalArgumentException("Cannot get type of locator" + locator_type);
         }
     }
+
+    //ожидание, что элемент не присутствует
+    public Boolean waitForElementNotPresent(String locator, String error, long timeOutInSeconds)
+    {
+        //преобразует строку локатора из метода в в объект By
+        By by = this.getLocatorString(locator);
+        //Создает экземпляр класса WebDriverWait
+        WebDriverWait wait = new WebDriverWait(driver,timeOutInSeconds);
+        wait.withMessage(error + "\n");
+        //блокировка выполнения до выполнения условия
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
+    }
+
+    //ожидание, что элемент присутствует с выбором своего таймаута
+    public WebElement waitForElementPresent(String locator, String error, long timeOutInSeconds)
+    {   //преобразует строку локатора из метода в в объект By
+        By by = this.getLocatorString(locator);
+        //Создает экземпляр класса WebDriverWait
+        WebDriverWait wait = new WebDriverWait(driver,timeOutInSeconds);
+        wait.withMessage(error + "\n");
+        //блокировка выполнения до выполнения условия
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    //дефолтное ожидание, что элемеент присутствует - время 5 сек
+    public WebElement waitForElementPresent(String locator, String error)
+    {
+        return waitForElementPresent(locator, error, 5);
+    }
+
+    //ожидание, что элемент присутствует и клик по нему
+    public WebElement waitForElementPresentAndClick(String locator, String error, long timeOutInSeconds)
+    {
+        //Возврашает веб элемент из метода waitForElementPresent
+        WebElement element = waitForElementPresent(locator, error, timeOutInSeconds);
+        element.click();
+        return element;
+    }
+
+    //ожидание, что элемент присутствует и ввод строки
+    public WebElement waitForElementAndSendKeys(String locator, String value, String error, long timeOutInSeconds)
+    {
+        //Возврашает веб элемент из метода waitForElementPresent
+        WebElement element = waitForElementPresent(locator, error, timeOutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+
+    //ожидание, что элемент не присутствует и очистка строки
+    public WebElement waitForElementAndClear(String locator, String error, long timeOutInSeconds)
+    {
+        //Возврашает веб элемент из метода waitForElementPresent
+        WebElement element = waitForElementPresent(locator, error, timeOutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    //Нажать на кнопку Back в МП
+    public void clickBackButton()
+    {
+        waitForElementPresentAndClick(BUTTON_BACK,"Cannot click button Back", 10);
+    }
+    //выполнить Enter, нажать Enter на клавиатуре телефона
+    public void sendEnter()
+    {
+        driver.executeScript("mobile:performEditorAction", ImmutableMap.of("action","done"));
+    }
+
+    //Свайп вверх
+    public void swipeUP(int timeOut)
+    {
+        //Создание объеката TouchAction
+        TouchAction action = new TouchAction(driver);
+        //Получениов размеры текущего окна (экрана) устройства (ширина и высота в пикселях)
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width/2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+        action.press(x,start_y).waitAction(timeOut).moveTo(x,end_y).release().perform();
+    }
+
+    //Быстрый свайп
+    public void swipeUpQuick()
+    {
+        swipeUP(300);
+    }
+
+    //свайп, пока не будет найден элемент
+    public void swipeUpAndFindElement(String locator, String error, int max_swipes)
+    {
+        //преобразует строку локатора из метода в в объект By
+        By by = this.getLocatorString(locator);
+        int already_swiped = 0;
+        while (driver.findElements(by).size() == 0) {
+            if (already_swiped > max_swipes)
+            {
+                waitForElementPresent(locator, "Cannot find element by swiping up. \n" + error, 0);
+                return;
+            }
+            swipeUpQuick();
+            ++already_swiped;
+        }
+
+    }
+
+    //Свайп налево от определенного элемента
+    public void swipeLeft(String locator, String error) throws InterruptedException
+    {
+        //Возврашает веб элемент из метода waitForElementPresent
+        WebElement element = waitForElementPresent(locator, error,10);
+        int left_x = element.getLocation().getX();
+        int right_x = left_x + element.getSize().getWidth();
+        int upper_y = element.getLocation().getY();
+        int lower_y = upper_y + element.getSize().getHeight();
+        int middle_y = (upper_y + lower_y)/2;
+        TouchAction action = new TouchAction(driver);
+        action.press(right_x, middle_y).waitAction(150).moveTo(left_x,middle_y).release().perform().waitAction(900);
+    }
+
 
 }
